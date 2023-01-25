d31c3be5daff52bf549d55903b0af327a65cc024
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-01-25 20:18:45 +0100

    download sketches function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 import os
-from datetime import datetime
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,24 +16,34 @@ AMOUNT = 5000
 ITERS = int(AMOUNT / VISIBLE_SKETCKES) + 1
 
 
-def write_csv(set_links, file_name):
-    df = pd.DataFrame(set_links)
+def write_csv(data_set, file_name, headers=["link"]):
+    df = pd.DataFrame(data_set)
 
     if not os.path.exists('links'):
         os.makedirs('links')
 
-    df.to_csv(file_name)
+    df.to_csv(file_name, header=headers)
 
 
-def collect_links():
+def load_csv(file_name, headers=None):
+    data = pd.read_csv(file_name, names=headers)
+    set_links = set(data["link"])
+    return set_links
+
+
+def collect_links(iters, file_name):
+    driver.get("https://openprocessing.org/browse/?time=anytime&type=all&q=#")
+    assert "Browse Sketches - OpenProcessing" in driver.title
+
     array_links = set()
 
     wait.until(EC.visibility_of_element_located(
         (By.CLASS_NAME, "sketchThumbContainer")))
 
     # show more
-    for i in range(ITERS):
-        element = wait.until(EC.element_to_be_clickable((By.ID, 'showMoreButton')))
+    for i in range(iters):
+        element = wait.until(
+            EC.element_to_be_clickable((By.ID, 'showMoreButton')))
         element.click()
 
     links = driver.find_elements(By.CLASS_NAME, "sketchThumbContainer")
@@ -48,38 +57,84 @@ def collect_links():
             print(f'Error en collect_links {link}')
 
     print(f"Numero de links: {len(array_links)}")
-    write_csv(array_links, "./links/all_links.csv")
+    write_csv(array_links, file_name)
+
+
+def get_sketch(link):
+    try:
+        driver.get(link)
+
+        likes = wait.until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@data-target='#heartSidePanel'][@class='metric']")))
+        likes_amount = likes.text
+
+        comments = wait.until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@data-target='#commentSidePanel'][@class='metric']")))
+        comments_amount = comments.text
+
+        wait.until(EC.element_to_be_clickable(
+            (By.CLASS_NAME, 'icon_share'))).click()
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//a[@rel='nofollow']"))).click()
+
+        return (link, likes_amount, comments_amount)
+
+    except:
+        print(f'Error en abrir {link}')
+
+
+def download_sketches(filename):
+    links = load_csv('links/some.csv', headers=["link"])
+
+    sketches = set()
+
+    for link in links:
+        t = get_sketch(link)
+        if t is not None:
+            sketches.add(t)
+
+    # t = get_sketch('https://openprocessing.org/sketch/453716')
+    # t = get_sketch('https://openprocessing.org/sketch/423716')
+
+    write_csv(sketches, "./links/links-data.csv", ["link", "likes", "comments"])
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
 
     driver = webdriver.Firefox(options=options)
     driver.maximize_window()
-    driver.get("https://openprocessing.org/browse/?time=anytime&type=all&q=#")
-
-    print(f'Title: {driver.title}')
-    assert "Browse Sketches - OpenProcessing" in driver.title
-
     wait = WebDriverWait(driver, 10)
 
+    driver.get("https://openprocessing.org/signin")
+
     # accept cookies
-    element = wait.until(EC.visibility_of_element_located((By.ID, 'ccc-notify-accept')))
+    element = wait.until(EC.visibility_of_element_located(
+        (By.ID, 'ccc-notify-accept')))
     element.click()
 
-    # collect_links()
+    # sign in
+    driver.find_element(By.NAME, "username").send_keys(
+        "***")
+    driver.find_element(By.NAME, "password").send_keys("***")
+
+    wait.until(EC.element_to_be_clickable(
+        (By.ID, 'joinModal_submitButton'))).click()
+
+    # comments = driver.find_element(
+    #   By.XPATH, "//div[@data-target='#commentSidePanel'][@class='metric']")
+
+    time.sleep(2)
+
+    ###
+    # collect_links(0, "./links/some.csv")
+
+    ###
+    download_sketches('/links/some.csv')
+
+    driver.close()
 
     print("--- %s seconds ---" % (time.time() - start_time))
-
-
-    # Abre el sketch
-    # driver.get('https://openprocessing.org/sketch/1797657')
-
-    # element = driver.find_elements(By.CLASS_NAME, "metricGroup")
-    # element.click()
-    # print(element)
-    # a = driver.find_elements(By.CLASS_NAME, "icon_share white").click()
-    # b = driver.find_elements_by_class_name("icon icon_share white").click()
