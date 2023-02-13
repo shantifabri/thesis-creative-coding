ab367617c64310a8e363ff41ae18439fc9152dde
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-02-13 19:20:12 +0100

    main script updates

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SKETCKES_PER_ROW = 12
 NUM_ROWS = 10
 VISIBLE_SKETCKES = SKETCKES_PER_ROW * NUM_ROWS
 
-AMOUNT = 5000
+AMOUNT = 40000
 
 ITERS = int(AMOUNT / VISIBLE_SKETCKES) + 1
 
@@ -26,13 +26,16 @@ def write_csv(data_set, file_name, headers=["link"]):
 
 
 def load_csv(file_name, headers=None):
-    data = pd.read_csv(file_name, names=headers)
+    data = pd.read_csv(file_name, header=0, names=headers)
     set_links = set(data["link"])
     return set_links
 
 
 def collect_links(iters, file_name):
-    driver.get("https://openprocessing.org/browse/?time=anytime&type=all&q=#")
+    # hearted on month - "https://openprocessing.org/browse/#"
+    # created all time - "https://openprocessing.org/browse/?time=anytime&type=all&q=#"
+    # hearted all time - "https://openprocessing.org/browse/?time=anytime&type=hearts"
+    driver.get("https://openprocessing.org/browse/?time=anytime&type=hearts")
     assert "Browse Sketches - OpenProcessing" in driver.title
 
     array_links = set()
@@ -61,8 +64,14 @@ def collect_links(iters, file_name):
 
 
 def get_sketch(link):
+
     try:
         driver.get(link)
+
+        wait.until(EC.element_to_be_clickable(
+            (By.CLASS_NAME, 'icon_share'))).click()
+        wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//a[@rel='nofollow']"))).click()
 
         likes = wait.until(EC.visibility_of_element_located(
             (By.XPATH, "//div[@data-target='#heartSidePanel'][@class='metric']")))
@@ -72,19 +81,14 @@ def get_sketch(link):
             (By.XPATH, "//div[@data-target='#commentSidePanel'][@class='metric']")))
         comments_amount = comments.text
 
-        wait.until(EC.element_to_be_clickable(
-            (By.CLASS_NAME, 'icon_share'))).click()
-        wait.until(EC.element_to_be_clickable(
-            (By.XPATH, "//a[@rel='nofollow']"))).click()
-
         return (link, likes_amount, comments_amount)
 
     except:
-        print(f'Error en abrir {link}')
+        print(f'Error in opening {link}')
 
 
-def download_sketches(filename):
-    links = load_csv('links/some.csv', headers=["link"])
+def download_sketches(filename_links, filename_data):
+    links = load_csv(filename_links, headers=["link"])
 
     sketches = set()
 
@@ -93,17 +97,15 @@ def download_sketches(filename):
         if t is not None:
             sketches.add(t)
 
-    # t = get_sketch('https://openprocessing.org/sketch/453716')
-    # t = get_sketch('https://openprocessing.org/sketch/423716')
-
-    write_csv(sketches, "./links/links-data.csv", ["link", "likes", "comments"])
+    write_csv(sketches, filename_data,
+              headers=["link", "likes", "comments"])
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
     options = Options()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
 
     driver = webdriver.Firefox(options=options)
     driver.maximize_window()
@@ -117,23 +119,19 @@ if __name__ == '__main__':
     element.click()
 
     # sign in
-    driver.find_element(By.NAME, "username").send_keys(
-        "***")
+    driver.find_element(By.NAME, "username").send_keys("***")
     driver.find_element(By.NAME, "password").send_keys("***")
 
     wait.until(EC.element_to_be_clickable(
         (By.ID, 'joinModal_submitButton'))).click()
 
-    # comments = driver.find_element(
-    #   By.XPATH, "//div[@data-target='#commentSidePanel'][@class='metric']")
-
-    time.sleep(2)
+    time.sleep(1)
 
     ###
-    # collect_links(0, "./links/some.csv")
+    collect_links(0, "./links/hearted.csv")
 
     ###
-    download_sketches('/links/some.csv')
+    # download_sketches('./links/tiny_hearted_c.csv', './links/links-data.csv')
 
     driver.close()
 
