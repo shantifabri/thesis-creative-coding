4610544eca23600b0edffc2ccb79738eb735038e
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-06-07 17:31:42 +0200

    stats_cr join

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import json
+import re
 import pandas as pd
+from collections import defaultdict
 
 
 def load_cloc(filename):
@@ -82,17 +84,17 @@ def write_dict(dict, filename):
 
 df_demo = load_json("../cr_report_demo.json")  # 6  --- len(df["reports"])
 df_demo2 = load_json("../cr_report_demo_2.json")  # 7  --- len(df["reports"])
-# df = load_json("../cr_report_created_half1.json") # 6010
-# df = load_json("../cr_report_created_half2.json") # 6112
-# df = load_json("../cr_report_hearted_half1.json") # 3311
-# df = load_json("../cr_report_hearted_half2.json") # 8341
+# df_created1 = load_json("../cr_report_created_half1.json")  # 6010
+# df_created2 = load_json("../cr_report_created_half2.json") # 6112
+# df_hearted1 = load_json("../cr_report_hearted_half1.json") # 3311
+# df_hearted2 = load_json("../cr_report_hearted_half2.json") # 8341
 
 
 def analyse_cr(data):
     reports = data["reports"]
 
     stats = {
-        "sketches_setup_draw": 0,   # see if file has those 2 funcs  --  done
+        "files_setup_draw": 0,   # count files that have setup+draw funcs  --  done
         "funcs_setup": 0,           # count total of "setup" funcs  --  done
         "funcs_draw": 0,            # count total of "draw" funcs  --  done
         "n_files": len(reports),    # count all files -- done
@@ -100,8 +102,7 @@ def analyse_cr(data):
         "sketches": set(),          # unique sketches -- done
         "files_names": set(),       # unique file names -- done
         "func_names": set(),        # unique functions names  --  done
-        # {sketch: [[filename, sloc, [name of funcs]]]} -- done
-        "files": {},
+        "files": {},                # {sketch: [[filename, sloc, [name of funcs]]]} -- done
         "sloc_logical": {'max': 0,
                          'min': 1000,
                          'sum': 0,
@@ -112,13 +113,15 @@ def analyse_cr(data):
     # each report represents a file
     for report in reports:
         txt = report["path"]
-        x = txt.rfind("\\")
-        filename = txt[x+1:]
-        stats["files_names"].add(filename)  # add file name
-        txt = txt[:x]
-        y = txt.rfind("\\")
-        sketchname = txt[y+1:]
+
+        match = re.search(r'sketch\d+', txt)
+        s, e = match.span()
+
+        sketchname = txt[s:e]
+        filename = txt[e+1:]
+
         stats["sketches"].add(sketchname)  # add sketch name
+        stats["files_names"].add(filename)  # add file name
 
         # file SLOC
         sloc_log_file = report["aggregate"]["sloc"]["logical"]
@@ -154,7 +157,7 @@ def analyse_cr(data):
                 stats["funcs_setup"] += 1
 
         if draw and setup:
-            stats["sketches_setup_draw"] += 1
+            stats["files_setup_draw"] += 1
 
     stats["sloc_logical"]["file_avg"] = stats["sloc_logical"]["sum"]/stats["n_files"]
 
@@ -164,15 +167,46 @@ def analyse_cr(data):
 # get highest and lowest of indexes
 
 
-stats = analyse_cr(df_demo2)
+stats = analyse_cr(df_demo)
+# stats2 = analyse_cr(df_demo2)
+# stats_created1 = analyse_cr(df_created1)
+# stats_created2 = analyse_cr(df_created2)
 
-print(stats)
 
-write_dict(stats, "myfile.json")
+def join_cr_stats(stats1, stats2):
 
-print("ssss ------------------------")
-ss = load_json("./myfile.json")
-print(ss)
+    joined_dict = {
+        "files_setup_draw": stats1["files_setup_draw"] + stats2["files_setup_draw"],                # add count of both stat_dicts
+        "funcs_setup": stats1["funcs_setup"] + stats2["funcs_setup"],                               # add count of both stat_dicts
+        "funcs_draw": stats1["funcs_draw"] + stats2["funcs_draw"],                                  # add count of both stat_dicts
+        "n_files": stats1["n_files"] + stats2["n_files"],                                           # add count of both stat_dicts
+        "funcs": stats1["funcs"] + stats2["funcs"],                                                 # concat list of lists
+        "sketches": stats1["sketches"].union(stats2["sketches"]),                                   # union sets of unique sketches
+        "files_names": stats1["files_names"].union(stats2["files_names"]),                          # union sets of unique file names
+        "func_names": stats1["func_names"].union(stats2["func_names"]),                             # union sets of unique functions names
+        "files": {**stats1["files"], **stats2["files"]},                                            # {sketch: [[filename, sloc, [name of funcs]]]} -- done
+        "sloc_logical": {'max': max(stats1["sloc_logical"]["max"], stats2["sloc_logical"]["max"]),  # max of both stat_dicts
+                         'min': min(stats1["sloc_logical"]["min"], stats2["sloc_logical"]["min"]),  # min of both stat_dicts
+                         'sum': stats1["sloc_logical"]["sum"] + stats2["sloc_logical"]["sum"],      #sum of both stat_dicts
+                         'file_avg': 0,
+                         'func_avg': 0}
+    }
+
+    joined_dict["sloc_logical"]["file_avg"] = joined_dict["sloc_logical"]["sum"] / joined_dict["n_files"]
+
+    # (avg1 * count1, avg2 *coun2) / (count1+count2)
+    sum1 = stats1["sloc_logical"]["func_avg"] * len(stats1["funcs"])
+    sum2 = stats2["sloc_logical"]["func_avg"] * len(stats2["funcs"])
+    total_funcs = len(stats1["funcs"]) + len(stats2["funcs"])
+    joined_dict["sloc_logical"]["func_avg"] = (sum1 + sum2) / total_funcs
+
+    return joined_dict
+
+
+# s = join_cr_stats(stats, stats2)
+# print(s)
+# write_dict(s, "test_join.json")
+
 
 def analyse_cr_stats(stats):
     # get loc's (max, min, sum for avg) ---->  get how many close to it
