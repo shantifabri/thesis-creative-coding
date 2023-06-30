fbf5837a967a9956d18f83452e27e04f1274601c
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-06-30 16:36:57 +0200

    get metrics function and cloc aggregated

--- analysis.py
+++ analysis.py
@@ -1,8 +1,13 @@
 import json
 import re
 import pandas as pd
-from collections import defaultdict
 
+n_sketches_created = 14542
+n_sketches_hearted = 14196
+n_sketches_total = n_sketches_created + n_sketches_hearted
+
+
+### CLOC ###
 
 def load_cloc(filename):
     data = pd.read_csv(filename, index_col='language')
@@ -18,6 +23,7 @@ def anylise_cloc(filecreated, filehearted, language):
     df_created = load_cloc(filecreated)
     df_hearted = load_cloc(filehearted)
 
+    # created
     tot_created = df_created.loc['SUM']['files']
     n_files_created = df_created.loc[language]['files']
     loc_created = df_created.loc[language]['code']
@@ -28,6 +34,7 @@ def anylise_cloc(filecreated, filehearted, language):
     percent_files_created = round(
         (int(n_files_created) / int(tot_created)) * 100, 2)
 
+    # hearted
     tot_hearted = df_hearted.loc['SUM']['files']
     n_files_hearted = df_hearted.loc[language]['files']
     loc_hearted = df_hearted.loc[language]['code']
@@ -38,23 +45,48 @@ def anylise_cloc(filecreated, filehearted, language):
     percent_files_hearted = round(
         (int(n_files_hearted) / int(tot_hearted)) * 100, 2)
 
-    data = [{'total files': tot_created,
-             f'{language} files': n_files_created,
-             f'% {language} files': percent_files_created,
-             f'total loc {language}': loc_created,
-             'avg loc per file': loc_per_file_created,
-             f'total comments {language}': lo_comms_created,
-             'avg comments per file': lo_comms_per_file_created},
-            {'total files': tot_hearted,
-             f'{language} files': n_files_hearted,
-             f'% {language} files': percent_files_hearted,
-             f'total loc {language}': loc_hearted,
-             'avg loc per file': loc_per_file_hearted,
-             f'total comments {language}': lo_comms_hearted,
-             'avg comments per file': lo_comms_per_file_hearted}]
+    # agregated
+    tot = tot_created + tot_hearted
+    n_files_lang = n_files_created + n_files_hearted
+    loc = loc_created + loc_hearted
+    lo_comms = lo_comms_created + lo_comms_hearted
+
+    loc_per_file = round(loc/n_files_lang, 2)
+    lo_comms_per_file = round(lo_comms/n_files_lang, 2)
+    percent_files = round((int(n_files_lang) / int(tot)) * 100, 2)
+
+    data = [
+        {'total files': tot_created,
+         f'{language} files': n_files_created,
+         f'% {language} files': percent_files_created,
+         f'avg {language} files per sketch': n_files_created/n_sketches_created,
+         f'total loc {language}': loc_created,
+         'avg loc per file': loc_per_file_created,
+         f'total comments {language}': lo_comms_created,
+         'avg comments per file': lo_comms_per_file_created},
+
+        {'total files': tot_hearted,
+         f'{language} files': n_files_hearted,
+         f'% {language} files': percent_files_hearted,
+         f'avg {language} files per sketch': n_files_hearted/n_sketches_hearted,
+         f'total loc {language}': loc_hearted,
+         'avg loc per file': loc_per_file_hearted,
+         f'total comments {language}': lo_comms_hearted,
+         'avg comments per file': lo_comms_per_file_hearted},
+
+        {'total files': tot,
+         f'{language} files': n_files_lang,
+         f'% {language} files': percent_files,
+         f'avg {language} files per sketch': n_files_lang/n_sketches_total,
+         f'total loc {language}': loc,
+         'avg loc per file': loc_per_file,
+         f'total comments {language}': lo_comms,
+         'avg comments per file': lo_comms_per_file}
+    ]
 
     # Lists of dictionaries and row index.
-    df = pd.DataFrame(data, index=['created', 'hearted'])
+    df = pd.DataFrame(data, index=['created', 'hearted', 'total'])
+    df.index.name = 'Group'
 
     write_csv(f'./analysis/cloc_analysis_{language}.csv', df)
 
@@ -63,7 +95,11 @@ def anylise_cloc(filecreated, filehearted, language):
 #              "./analysis/cloc_hearted_skip_unique.csv", 'JavaScript')
 # anylise_cloc("./analysis/cloc_created_skip_unique.csv",
 #              "./analysis/cloc_hearted_skip_unique.csv", 'Arduino Sketch')
+# anylise_cloc("./analysis/cloc_created_skip_unique.csv",
+#              "./analysis/cloc_hearted_skip_unique.csv", 'HTML')
 
+
+### CR - REPORT ###
 
 def load_json(filename):
     with open(filename, encoding="utf8") as f:
@@ -79,18 +115,11 @@ def json_serializer(value):
 
 def write_dict(dict, filename):
     with open(filename, 'w') as file:
-        file.write(json.dumps(dict, default=json_serializer))
+        file.write(json.dumps(dict, default=json_serializer, indent=2))
 
 
-df_demo = load_json("../cr_report_demo.json")  # 6  --- len(df["reports"])
-df_demo2 = load_json("../cr_report_demo_2.json")  # 7  --- len(df["reports"])
-# df_created1 = load_json("../cr_report_created_half1.json")  # 6010
-# df_created2 = load_json("../cr_report_created_half2.json") # 6112
-# df_hearted1 = load_json("../cr_report_hearted_half1.json") # 3311
-# df_hearted2 = load_json("../cr_report_hearted_half2.json") # 8341
-
-
-def analyse_cr(data):
+# sketches, files, funcs: names and counts - loc #
+def get_cr_stats(data):
     reports = data["reports"]
 
     stats = {
@@ -98,11 +127,14 @@ def analyse_cr(data):
         "funcs_setup": 0,           # count total of "setup" funcs  --  done
         "funcs_draw": 0,            # count total of "draw" funcs  --  done
         "n_files": len(reports),    # count all files -- done
-        "funcs": [],                # [[sketchname,func["name"],func["params"],func["sloc"]["logical"]]]
+        # [[sketchname,func["name"],func["params"],func["sloc"]["logical"]]] -- done
+        "funcs": [],
         "sketches": set(),          # unique sketches -- done
         "files_names": set(),       # unique file names -- done
         "func_names": set(),        # unique functions names  --  done
-        "files": {},                # {sketch: [[filename, sloc, [name of funcs]]]} -- done
+        # {sketch: [[filename, sloc, [name of funcs]]]} -- done
+        "files": {},
+        "params": data["params"],
         "sloc_logical": {'max': 0,
                          'min': 1000,
                          'sum': 0,
@@ -163,54 +195,226 @@ def analyse_cr(data):
 
     return stats
 
-# see if paths repeat - counter of files per sketch - path up till last \\
-# get highest and lowest of indexes
+
+# cyclomatic - cyclomaticDensity - maintainability - halstead #
+def get_cr_metrics(data):
+    reports = data["reports"]
+
+    stats = {
+        # whole thing
+        "n_modules": len(reports),
+        "n_funcs": 0,
+        "maintainability_mod_avg": data["maintainability"],  # avg per-module maintainability index
+        "cyclomatic_func_avg": data["cyclomatic"],           # avg per-func cyclomatic complexity
+        "effort_func_avg": data["effort"],                   # avg per-func Halstead effort
+
+        # per report (aggregate or out)
+        "maintainability_mod_list": [],      # maintainability index for the module
+        "cyclomatic_mod_list": [],           # agg -> cyclomatic complex for the module
+        "cyclomaticDensity_mod_list": [],    # agg -> cyclomatic complex density for the module
+        "effort_mod_func_avg_list": [],      # avg of Halstead effort of funcs in module
+        "cyclomatic_mod_func_avg_list": [],  # avg of cyclomatic complex of funcs in module
+
+        # per func in report
+        "cyclomatic_func_list": [],         # cyclomatic complex for the func
+        "cyclomaticDensity_func_list": [],  # cyclomatic complex density for the func
+
+        # halstead
+        # aggregate -- for the module
+        "vocabulary_mod_list": [],  # Halstead vocabulary size
+        "difficulty_mod_list": [],  # Halstead difficulty
+        "volume_mod_list": [],  # Halstead volume
+        "effort_mod_list": [],  # Halstead effort
+        "bugs_mod_list": [],  # Halstead bugs
+        "time_mod_list": [],  # Halstead time
+
+        # func -- for the func
+        "vocabulary_func_list": [],  # Halstead vocabulary size
+        "difficulty_func_list": [],  # Halstead difficulty
+        "volume_func_list": [],  # Halstead volume
+        "effort_func_list": [],  # Halstead effort
+        "bugs_func_list": [],  # Halstead bugs
+        "time_func_list": [],  # Halstead time
+    }
+
+    for report in reports:
+        stats["maintainability_mod_list"].append(report["maintainability"])
+        stats["cyclomatic_mod_list"].append(report["aggregate"]["cyclomatic"])
+        stats["cyclomaticDensity_mod_list"].append(report["aggregate"]["cyclomaticDensity"])
+        stats["effort_mod_func_avg_list"].append(report["effort"])
+        stats["cyclomatic_mod_func_avg_list"].append(report["cyclomatic"])
+
+        stats["vocabulary_mod_list"].append(report["aggregate"]["halstead"]["vocabulary"])
+        stats["difficulty_mod_list"].append(report["aggregate"]["halstead"]["difficulty"])
+        stats["volume_mod_list"].append(report["aggregate"]["halstead"]["volume"])
+        stats["effort_mod_list"].append(report["aggregate"]["halstead"]["effort"])
+        stats["bugs_mod_list"].append(report["aggregate"]["halstead"]["bugs"])
+        stats["time_mod_list"].append(report["aggregate"]["halstead"]["time"])
+
+        for func in report["functions"]:
+            stats["n_funcs"] += 1
+            stats["cyclomatic_func_list"].append(func["cyclomatic"])
+            stats["cyclomaticDensity_func_list"].append(func["cyclomaticDensity"])
+
+            stats["vocabulary_func_list"].append(func["halstead"]["vocabulary"])
+            stats["difficulty_func_list"].append(func["halstead"]["difficulty"])
+            stats["volume_func_list"].append(func["halstead"]["volume"])
+            stats["effort_func_list"].append(func["halstead"]["effort"])
+            stats["bugs_func_list"].append(func["halstead"]["bugs"])
+            stats["time_func_list"].append(func["halstead"]["time"])
+
+    return stats
 
 
-stats = analyse_cr(df_demo)
-# stats2 = analyse_cr(df_demo2)
-# stats_created1 = analyse_cr(df_created1)
-# stats_created2 = analyse_cr(df_created2)
-
-
-def join_cr_stats(stats1, stats2):
+def join_cr_stats(s1, s2):
 
     joined_dict = {
-        "files_setup_draw": stats1["files_setup_draw"] + stats2["files_setup_draw"],                # add count of both stat_dicts
-        "funcs_setup": stats1["funcs_setup"] + stats2["funcs_setup"],                               # add count of both stat_dicts
-        "funcs_draw": stats1["funcs_draw"] + stats2["funcs_draw"],                                  # add count of both stat_dicts
-        "n_files": stats1["n_files"] + stats2["n_files"],                                           # add count of both stat_dicts
-        "funcs": stats1["funcs"] + stats2["funcs"],                                                 # concat list of lists
-        "sketches": stats1["sketches"].union(stats2["sketches"]),                                   # union sets of unique sketches
-        "files_names": stats1["files_names"].union(stats2["files_names"]),                          # union sets of unique file names
-        "func_names": stats1["func_names"].union(stats2["func_names"]),                             # union sets of unique functions names
-        "files": {**stats1["files"], **stats2["files"]},                                            # {sketch: [[filename, sloc, [name of funcs]]]} -- done
-        "sloc_logical": {'max': max(stats1["sloc_logical"]["max"], stats2["sloc_logical"]["max"]),  # max of both stat_dicts
-                         'min': min(stats1["sloc_logical"]["min"], stats2["sloc_logical"]["min"]),  # min of both stat_dicts
-                         'sum': stats1["sloc_logical"]["sum"] + stats2["sloc_logical"]["sum"],      #sum of both stat_dicts
+        # add count of both stat_dicts
+        "files_setup_draw": s1["files_setup_draw"] + s2["files_setup_draw"],
+        # add count of both stat_dicts
+        "funcs_setup": s1["funcs_setup"] + s2["funcs_setup"],
+        # add count of both stat_dicts
+        "funcs_draw": s1["funcs_draw"] + s2["funcs_draw"],
+        # add count of both stat_dicts
+        "n_files": s1["n_files"] + s2["n_files"],
+        # concat list of lists
+        "funcs": s1["funcs"] + s2["funcs"],
+        # union sets of unique sketches
+        "sketches": s1["sketches"].union(s2["sketches"]),
+        # union sets of unique file names
+        "files_names": s1["files_names"].union(s2["files_names"]),
+        # union sets of unique functions names
+        "func_names": s1["func_names"].union(s2["func_names"]),
+        # {sketch: [[filename, sloc, [name of funcs]]]} -- done
+        "files": {**s1["files"], **s2["files"]},
+        "sloc_logical": {'max': max(s1["sloc_logical"]["max"], s2["sloc_logical"]["max"]),  # max of both stat_dicts
+                         # min of both stat_dicts
+                         'min': min(s1["sloc_logical"]["min"], s2["sloc_logical"]["min"]),
+                         # sum of both stat_dicts
+                         'sum': s1["sloc_logical"]["sum"] + s2["sloc_logical"]["sum"],
                          'file_avg': 0,
                          'func_avg': 0}
     }
 
-    joined_dict["sloc_logical"]["file_avg"] = joined_dict["sloc_logical"]["sum"] / joined_dict["n_files"]
+    joined_dict["sloc_logical"]["file_avg"] = joined_dict["sloc_logical"]["sum"] / \
+        joined_dict["n_files"]
 
     # (avg1 * count1, avg2 *coun2) / (count1+count2)
-    sum1 = stats1["sloc_logical"]["func_avg"] * len(stats1["funcs"])
-    sum2 = stats2["sloc_logical"]["func_avg"] * len(stats2["funcs"])
-    total_funcs = len(stats1["funcs"]) + len(stats2["funcs"])
+    sum1 = s1["sloc_logical"]["func_avg"] * len(s1["funcs"])
+    sum2 = s2["sloc_logical"]["func_avg"] * len(s2["funcs"])
+    total_funcs = len(s1["funcs"]) + len(s2["funcs"])
     joined_dict["sloc_logical"]["func_avg"] = (sum1 + sum2) / total_funcs
 
     return joined_dict
 
 
-# s = join_cr_stats(stats, stats2)
-# print(s)
-# write_dict(s, "test_join.json")
+def join_cr_metrics(m1, m2):
+
+    joined_dict = {
+        "n_modules": m1["n_modules"] + m2["n_modules"],
+        "n_funcs": m1["n_funcs"] + m2["n_funcs"],
+        "maintainability_mod_avg": 0,  # avg per-module maintainability index
+        "cyclomatic_func_avg": 0,      # avg per-func cyclomatic complexity
+        "effort_func_avg": 0,          # avg per-func Halstead effort
+
+        # per report (aggregate or out)
+        "maintainability_mod_list": [*m1["maintainability_mod_list"], *m2["maintainability_mod_list"]],      # maintainability index for the module
+        "cyclomatic_mod_list": [*m1["cyclomatic_mod_list"], *m2["cyclomatic_mod_list"]],           # agg -> cyclomatic complex for the module
+        "cyclomaticDensity_mod_list": [*m1["cyclomaticDensity_mod_list"], *m2["cyclomaticDensity_mod_list"]],    # agg -> cyclomatic complex density for the module
+        "effort_mod_func_avg_list": [*m1["effort_mod_func_avg_list"], *m2["effort_mod_func_avg_list"]],      # avg of Halstead effort of funcs in module
+        "cyclomatic_mod_func_avg_list": [*m1["cyclomatic_mod_func_avg_list"], *m2["cyclomatic_mod_func_avg_list"]],  # avg of cyclomatic complex of funcs in module
+
+        # per func in report
+        "cyclomatic_func_list": [*m1["cyclomatic_func_list"], *m2["cyclomatic_func_list"]],         # cyclomatic complex for the func
+        "cyclomaticDensity_func_list": [*m1["cyclomaticDensity_func_list"], *m2["cyclomaticDensity_func_list"]],  # cyclomatic complex density for the func
+
+        # halstead
+        # aggregate -- for the module
+        "vocabulary_mod_list": [*m1["vocabulary_mod_list"], *m2["vocabulary_mod_list"]],  # Halstead vocabulary size
+        "difficulty_mod_list": [*m1["difficulty_mod_list"], *m2["difficulty_mod_list"]],  # Halstead difficulty
+        "volume_mod_list": [*m1["volume_mod_list"], *m2["volume_mod_list"]],  # Halstead volume
+        "effort_mod_list": [*m1["effort_mod_list"], *m2["effort_mod_list"]],  # Halstead effort
+        "bugs_mod_list": [*m1["bugs_mod_list"], *m2["bugs_mod_list"]],  # Halstead bugs
+        "time_mod_list": [*m1["time_mod_list"], *m2["time_mod_list"]],  # Halstead time
+
+        # func -- for the func
+        "vocabulary_func_list": [*m1["vocabulary_func_list"], *m2["vocabulary_func_list"]],  # Halstead vocabulary size
+        "difficulty_func_list": [*m1["difficulty_func_list"], *m2["difficulty_func_list"]],  # Halstead difficulty
+        "volume_func_list": [*m1["volume_func_list"], *m2["volume_func_list"]],  # Halstead volume
+        "effort_func_list": [*m1["effort_func_list"], *m2["effort_func_list"]],  # Halstead effort
+        "bugs_func_list": [*m1["bugs_func_list"], *m2["bugs_func_list"]],  # Halstead bugs
+        "time_func_list": [*m1["time_func_list"], *m2["time_func_list"]],  # Halstead time
+
+    }
+
+    joined_dict["maintainability_mod_avg"] = ((m1["maintainability_mod_avg"]*m1["n_modules"]) + (m2["maintainability_mod_avg"]*m2["n_modules"])) / joined_dict["n_modules"]
+    joined_dict["cyclomatic_func_avg"] = ((m1["cyclomatic_func_avg"]*m1["n_funcs"]) + (m2["cyclomatic_func_avg"]*m2["n_funcs"])) / joined_dict["n_funcs"]
+    joined_dict["effort_func_avg"] = ((m1["effort_func_avg"]*m1["n_funcs"]) + (m2["effort_func_avg"]*m2["n_funcs"]))/ joined_dict["n_funcs"]
+
+    return joined_dict
+
+
+## Load cr_reports
+# df_demo = load_json("../cr_report_demo.json")  # 6  --- len(df["reports"])
+# df_demo2 = load_json("../cr_report_demo_2.json")  # 7  --- len(df["reports"])
+df_created1 = load_json("../cr_report_created_half1.json")  # 6010
+df_created2 = load_json("../cr_report_created_half2.json")  # 6112
+df_hearted1 = load_json("../cr_report_hearted_half1.json")  # 3311
+df_hearted2 = load_json("../cr_report_hearted_half2.json")  # 8341
+
+
+# stats = get_cr_stats_stats(df_demo)
+# stats2 = get_cr_stats_stats(df_demo2)
+# stats_created1 = get_cr_stats_stats(df_created1)
+# stats_created2 = get_cr_stats_stats(df_created2)
+
+# metrics = get_cr_metrics(df_demo)
+# metrics2 = get_cr_metrics(df_demo2)
+
+## Stats
+# stats_created1 = get_cr_stats(df_created1)
+# stats_created2 = get_cr_stats(df_created2)
+
+# stats_hearted1 = get_cr_stats(df_hearted1)
+# stats_hearted2 = get_cr_stats(df_hearted2)
+
+# stats_created_joined = join_cr_stats(stats_created1, stats_created2)
+# stats_hearted_joined = join_cr_stats(stats_hearted1, stats_hearted2)
+# write_dict(stats_created_joined, "stats_created_joined.json")
+# write_dict(stats_hearted_joined, "stats_hearted_joined.json")
+
+# stats_total_joined = join_cr_stats(stats_created_joined, stats_hearted_joined)
+# write_dict(stats_total_joined, "stats_total_joined.json")
+
+
+## Metrics
+# metrics_created1 = get_cr_metrics(df_created1)
+# metrics_created2 = get_cr_metrics(df_created2)
+
+# metrics_hearted1 = get_cr_metrics(df_hearted1)
+# metrics_hearted2 = get_cr_metrics(df_hearted2)
+
+# metrics_created_joined = join_cr_metrics(metrics_created1, metrics_created2)
+# metrics_hearted_joined = join_cr_metrics(metrics_hearted1, metrics_hearted2)
+# write_dict(metrics_created_joined, "metrics_created_joined.json")
+# write_dict(metrics_hearted_joined, "metrics_hearted_joined.json")
+
+# metrics_total_joined = join_cr_metrics(metrics_created_joined, metrics_hearted_joined)
+# write_dict(metrics_total_joined, "metrics_total_joined.json")
 
 
 def analyse_cr_stats(stats):
     # get loc's (max, min, sum for avg) ---->  get how many close to it
     # how many funcs per project, max, min, avg
+    # get highest and lowest of indexes
+    #
+    pass
+
+
+def analyse_cr_metrics(metrics):
+    # from avg ---->  get how many close to it
+    # get highest and lowest of indexes
+    #
     pass
 
 
